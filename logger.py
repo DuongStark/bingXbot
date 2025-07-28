22aec3bec5db737a3d900aa35b8d86a0f38796fc
DuongStark <starkduong@gmail.com>
2025-07-28 19:00:25 +0700

    xong chua

--- logger.py
+++ logger.py
@@ -1,5 +1,4 @@
 from datetime import datetime
 
 def log_event(event):
-    with open("trade_log.txt", "a", encoding="utf-8") as f:
-        f.write(f"[{datetime.now()}] {event}\n") 
+    print(f"[{datetime.now()}] {event}") 
